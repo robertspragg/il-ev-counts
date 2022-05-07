a27716b2eab467bf103952b00b8a24ab4decee8d
Robert Spragg <roberts@eiqmobility.com>
2022-05-07 15:55:35 -0500

    added check of total with reported total; logging

--- query_registration_data.py
+++ query_registration_data.py
@@ -7,7 +7,9 @@ import pandas as pd
 from io import BytesIO
 import pdfplumber
 import re
+import logging
 
+LOGGER = logging.getLogger()
 SOS_EV_FILE = 'https://www.ilsos.gov/departments/vehicles/statistics/electric/home.html'
 
 # TODO: download and save to data directory
@@ -37,8 +39,14 @@ for page in pdf.pages[1:]:
 
 # additional cleanup
 ev_df['municipality'] = ev_df['municipality'].str.strip()
+
+# extract total vehicle count from end of file for sanity checking
+total_ev_count = re.sub(r'(.+\s)', '', ev_df.tail(1)['zip_code'].values[0])
+
 ev_df = ev_df[~ev_df['ev_count'].isna()]
 ev_df['ev_count'] = ev_df['ev_count'].astype(int)
 
-# TODO: check that total count matches total count in PDF
+if ev_df['ev_count'].sum() != int(total_ev_count):
+    LOGGER.warning(f"Derived EV count of {ev_df['ev_count'].sum()} does not match provided total of {total_ev_count}")
+
 ev_df.to_csv(cwd + '/data/ev_counts_by_zipcode.csv', index=False)
