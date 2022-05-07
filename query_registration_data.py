5230b876809e78d6fc09db9ede6d333dbf3c7e41
Robert Spragg <roberts@eiqmobility.com>
2022-05-07 13:15:23 -0500

    first draft of code that takes PDF to CSV done

--- query_registration_data.py
+++ query_registration_data.py
@@ -1,10 +1,9 @@
-"""
-Given a specific file path on Secretary of State website, downloads file, parses data, and saves ZIP-code level EV data
+""" Given a specific file path on Secretary of State website, downloads file,
+parses data, and saves ZIP-code level EV data
 """
 import os
 
 import pandas as pd
-import pycurl
 from io import BytesIO
 import pdfplumber
 import re
@@ -12,29 +11,34 @@ import re
 SOS_EV_FILE = 'https://www.ilsos.gov/departments/vehicles/statistics/electric/home.html'
 
 # TODO: download and save to data directory
-buffer = BytesIO()
-c = pycurl.Curl()
-c.setopt(c.URL, SOS_DIR)
+# buffer = BytesIO()
+# c = pycurl.Curl()
+# c.setopt(c.URL, SOS_DIR)
 
 # TODO: add linting
 
-# TODO: read from correct data path
-pdf_data = '/Users/robert/Learnings/il-ev-counts/data/electric041522.pdf'
+cwd = os.getcwd()
+pdf_data = 'electric041522.pdf'
 
 # extract EV counts by ZIP code
-pdf = pdfplumber.open(pdf_data)
+pdf = pdfplumber.open(cwd + f'/data/{pdf_data}')
 ev_df = pd.DataFrame(columns=['municipality', 'zip_code', 'ev_count'])
-# TODO first two pages need special treatment
-# parse pages with uniform layout
-for page in pdf.pages[2:]:
+
+# parse pages with uniform layout - skipping first page
+for page in pdf.pages[1:]:
     text = page.extract_text()
     lines = text.split('\n')
-    line_split = [re.split(r'\s{3,}', line) for line in lines]
-    temp_df = pd.DataFrame(line_split, columns=['district', 'zip_code', 'ev_count'])
+    line_split = [re.split(r'\b\s{2,}', line) for line in lines]
+    temp_df = pd.DataFrame(line_split, columns=['municipality', 'zip_code', 'ev_count'])
+    cutoff_idx = temp_df.index[temp_df['municipality'].str.contains('ZIPCODE TOTALS', na=False)].tolist()
+    if cutoff_idx:
+        temp_df = temp_df.loc[cutoff_idx[0] + 1:]
     ev_df = pd.concat([ev_df, temp_df])
 
-# TODO: remove whitespace
-# TODO: remove last row
+# additional cleanup
+ev_df['municipality'] = ev_df['municipality'].str.strip()
+ev_df = ev_df[~ev_df['ev_count'].isna()]
+ev_df['ev_count'] = ev_df['ev_count'].astype(int)
 
-
-# TODO: save final file as a csv
+# TODO: check that total count matches total count in PDF
+ev_df.to_csv(cwd + '/data/ev_counts_by_zipcode.csv', index=False)
